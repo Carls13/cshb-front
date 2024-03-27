7d937b8bd6220d21ca61bd72782ae501853c9f05
Carlos Hernandez <carlos.hernandez@validatedid.com>
2024-03-26 22:44:41 -0400

    fix: fetch Audiophile on portfolio load

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -4,6 +4,7 @@ import { MyHead as Head } from "../../components/Head/Head";
 export async function getServerSideProps() {
     // Fetch data from external API
     const res = await fetch(`https://api.carlosshb.com/portfolio/`);
+    fetch(`https://audiophile-backend-alx9.onrender.com/products`);
     const data = await res.json();
 
     // Pass data to the page via props
